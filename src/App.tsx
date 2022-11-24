1431af3dbd403906f27c0afe4e0dcd2f3daead1c
Nikita Shevchik <shevchik.nikita@gmail.com>
2022-11-24 18:02:24 +0300

    styled-components-learn

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Route } from 'react-router';
 import { Routes } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 import './App.scss';
 import Counter from './pages/Counter/Counter';
 import Main from './pages/Main/Main';
 import State from './pages/State/State';
 
+const GlobalStyle = createGlobalStyle`
+body {
+  background-color: #4572; 
+}
+`;
+
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={<Main />}>
-          <Route element={<Counter />} path='counter' />
-          <Route element={<State />} path='state' />
-        </Route>
-      </Routes>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Main />}>
+            <Route element={<Counter />} path='counter' />
+            <Route element={<State />} path='state' />
+          </Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- src/pages/Counter/Counter.tsx
+++ src/pages/Counter/Counter.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import Button from '../../components/Button/Button'
 import useStore from '../../zustand/zustand'
 
-
 const SCounter = styled.div`
     font-size: 30px;
-    padding: 30px 0;
+    padding: 20px 0;
     font-weight: 700;
 `
 const SInput = styled.input`
@@ -15,8 +14,41 @@ const SInput = styled.input`
     border-radius: 5px;
     border: 1px solid #000;
     padding: 5px;
+    background-color: ${props => props.theme.bg};
+    color: ${props => props.theme.col};
 `
 
+const themes = {
+    dark: {
+        bg: "#000",
+        col: "red"
+    },
+    blue: {
+        bg: "skyblue",
+        col: "#000",
+    }
+}
+
+const rotate = keyframes`
+    0%{
+        transform: rotate(0deg);
+    }
+    
+    100%{
+        transform: rotate(360deg);
+    }
+`
+const SRotate = styled.div`
+    display: inline-block;
+    animation: ${rotate} 2s linear infinite;
+`
+
+const STitle = styled.div`
+    font-size: 50px;
+    &.red{
+        color: red;
+    }
+`
 
 const Counter = () => {
     const { count, inc, dec, dep, noDep, beb, bebr, setBeb } = useStore()
@@ -25,15 +57,16 @@ const Counter = () => {
 
     return (
         <SCounter>
-            <div>{count}</div>
+            <STitle className='red' >{count}</STitle>
             <Button cb={() => beb()} text="beb"></Button>
             {bebr}
+            <SRotate>TEST</SRotate>
             <Button text="+1" cb={() => inc()}></Button>
             <Button text="-1" cb={() => dec()}></Button>
             <Button text="+40" cb={() => dep(40)}></Button>
-            <SInput type="number" placeholder='Number...' value={how} onChange={(event) => setHow(event.target.value)} />
+            <SInput theme={themes.blue} type="number" placeholder='Number...' value={how} onChange={(event) => setHow(event.target.value)} />
             <Button text={`-${how}`} cb={() => noDep(Number(how))} ></Button>
-            <SInput type="text" placeholder='Your beb...' value={bebHow} onChange={(event) => setBebHow(event.target.value)} />
+            <SInput theme={themes.dark} type="text" placeholder='Your beb...' value={bebHow} onChange={(event) => setBebHow(event.target.value)} />
             <Button cb={() => setBeb(bebHow)} text="custom beb"></Button>
         </SCounter >
     )
